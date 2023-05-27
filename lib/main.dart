3867a0bd75af2ce827b9806d94f8364ad45f4c56
Sahar Sarraj <saharsarraj20.ss@gmail.com>
2023-05-27 22:27:21 +0100

    testing commit to master branch

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
     super.key,
   });
 
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
