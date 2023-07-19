e8763463eed3d46c30bba12fbe0058c31cfa5b8a
Sahar Sarraj <saharsarraj20.ss@gmail.com>
2023-07-19 17:31:59 +0100

    added AmplifyDataStore plugin

--- lib/services/amplify_services.dart
+++ lib/services/amplify_services.dart
@@ -1,29 +1,25 @@
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:baby_cry/amplifyconfiguration.dart';
+import 'package:baby_cry/models/ModelProvider.dart';
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
 
-class AmplifyService {
-  static configureAmplify() async {
-    // Add Pinpoint and Cognito Plugins, or any other plugins you want to use
-    // AmplifyAPI apiPlugin = AmplifyAPI();
-    AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
-    // AmplifyDataStore datastorePlugin = AmplifyDataStore(
-    // modelProvider: ModelProvider.instance,);
-
-    // AmplifyStorageS3 amplifyStorageS3 = AmplifyStorageS3();
-    Amplify.addPlugins([
-      // datastorePlugin,
-      // amplifyStorageS3,
-      authPlugin,
-      // apiPlugin,
-    ]);
-    // Once Plugins are added, configure Amplify
-    // Note: Amplify can only be configured once.
-    try {
-      await Amplify.configure(amplifyconfig);
-    } catch (e) {
-      print(
-          "Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
-    }
+//configuring amplify : adding all required amplify plugins
+Future<void> configureAmplify() async {
+  await Amplify.addPlugins([
+    // Create the Auth plugin.
+    AmplifyAuthCognito(),
+    // Added the Amplify DataStore (on-device storage engine that synchronizes data between the mobile phone and the database in the AWS Cloud)
+    AmplifyDataStore(modelProvider: ModelProvider.instance),
+    // Create the API plugin.
+    AmplifyAPI(modelProvider: ModelProvider.instance),
+  ]);
+  // Once Plugins are added, configure Amplify
+  // Note: Amplify can only be configured once.
+  try {
+    await Amplify.configure(amplifyconfig);
+  } catch (e) {
+    print("Amplify is already configured");
   }
 }
